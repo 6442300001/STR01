a77d678e5aa3b2c6fabbedb99cadd67b8fcf8c99
Chow♥ <644230001@webmail.npru.ac.th>
2023-12-15 15:00:17 +0700

    commit

--- Home.py
+++ Home.py
@@ -81,7 +81,7 @@ if st.button("ทำนายผล"):
       #st.image("./pic/iris2.jpg")
       st.header("Versicolor")
    else:
-      #st.image("./pic/iris1.jpg")  
+      #st.image("./pic/iris3.jpg")  
       st.header("Verginiga")
    st.button("ไม่ทำนายผล")
 else :
