08c3cc32ec27f2a40dbedbf03418dceea8062c44
Chow♥ <644230001@webmail.npru.ac.th>
2023-12-15 15:19:36 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -75,7 +75,7 @@ if st.button("ทำนายผล"):
    out=Knn_model.predict(x_input)
 
    if out[0]=="Setosa":
-      #st.image("./pic/iris.jpg")
+      #st.image("./pic/Iris.PNG")
       st.header("Setosa")
    elif out[0]=="Versicolor":
       #st.image("./pic/iris2.jpg")
